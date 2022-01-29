9d3871d0a6cfb9a822a5efb03e66a39faa2ac8c2
keycomputer <keycomputereducation@gmail.com>
2022-01-29 19:33:17 +0530

    1

--- array2.c
+++ array2.c
@@ -2,8 +2,8 @@
 int main()
 {
 	float perc[100];
-	float sum =0 , avg;
-	int n, i;	
+	float sum =0 , avg, min, max;
+	int n, i, minroll, maxroll;	
 	printf("Enter no of students :: ");
 	scanf("%d", &n);
 	
@@ -13,10 +13,23 @@ int main()
 		scanf("%f",&perc[i]);
 		sum += perc[i];
 	}
+	min = max = perc[0]; 	
 	avg = sum / n;
 	for(i= 0;i<n;i++)
 	{
 		printf("Percentage of %d :: %f\n", i+1, perc[i]);
+		if (min > perc[i])
+		{
+			min = perc[i];
+			minroll = i;
+		}
+		if (max < perc[i])
+		{
+			max = perc[i];
+			maxroll = i;
+		}
 	}
 	printf("Average  = %0.2f", avg);
+	printf("Max Percentage = %0.2f - Rollno = %d", max, maxroll+1);
+	printf("Min Percentage = %0.2f - Rollno = %d", min, minroll+1);
 }
