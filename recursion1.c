f6ef430ce5cf4ddef616d8628c4daa26ca946d96
keycomputer <keycomputereducation@gmail.com>
2022-04-30 12:51:45 +0530

    d

--- recursion1.c
+++ recursion1.c
@@ -1,34 +1,53 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 
-//int isprime(int n1, int n2)
-//{
-//	if (n1==n2)
-//		return 1;
-//	else if (n2 % n1 == 0)
-//		return 0;
-//	else
-//		return(isprime(n1+1, n2));
-//}
-//int n1 = 2;
-int isprime(int n2)
+void doorandkey(char arr[])
 {
-	static int n1 = 2;
-	if (n1==n2)
-		return 1;
-	else if (n2 % n1 == 0)
-		return 0;
-	else{n1++;
-		return(isprime(n2));
-	}
+    int i,j;
+    for (i=0;arr[i]!='\0';i++)
+    {
+        //printf("%c",arr[i]);
+        if(islower(arr[i]))
+        {
+            for(j=i+1;arr[j]!='\0';j++)
+            {
+                if (toupper(arr[i]) == arr[j])
+                    break;
+            }
+            
+            if(j == strlen(arr)){
+                printf("NO\n");
+                return;
+            }
+           // printf("%d \n", j);
+        }
+        else if(isupper(arr[i]))
+        {
+            j = i-1;
+          for(j=i-1;j>=0;j--)
+            {
+                if (tolower(arr[i]) == arr[j])
+                    break;
+            }
+            if (j==-1){
+                printf("NO\n");
+                return;
+            }
+        }
+    }
+    printf("YES\n");
 }
+
 int main()
 {
-	int num;
-	printf("Enter any number ");
-	scanf("%d", &num);
-	if (isprime(num) == 1)
-		printf("PRime number ");
-	else
-		printf("not a prime number ");
-	return 0;
+    int n, i;
+    char arr[100];
+    scanf("%d\n",&n);
+    for(i=0;i<n;i++)
+    {
+    fgets(arr,99, stdin);
+    doorandkey(arr);
+    }
+
 }
--- unique.c
+++ unique.c
@@ -3,23 +3,26 @@
 
 int main()
 {
-	int arr[100], n, i, j, count=0;
+	// int arr[100], n, i, j, count=0;
 	
-	printf("Enter n :: ");
-	scanf("%d", &n);
-	printf("Enter elements ::\n ");
-	for(i=0;i<n;i++)
-		scanf("%d",&arr[i]);
-	for (i=0;i<n;i++)
-	{
-		count=1;
-		for(j=0;j<n;j++)
-		{
-			if (arr[i]== arr[j] && i !=j)
-			  count++;
-		}
-		if (count == 1)
-			printf("%d   ", arr[i]);
-	}
+	// printf("Enter n :: ");
+	// scanf("%d", &n);
+	// printf("Enter elements ::\n ");
+	// for(i=0;i<n;i++)
+	// 	scanf("%d",&arr[i]);
+	// for (i=0;i<n;i++)
+	// {
+	// 	count=1;
+	// 	for(j=0;j<n;j++)
+	// 	{
+	// 		if (arr[i]== arr[j] && i !=j)
+	// 		  count++;
+	// 	}
+	// 	if (count == 1)
+	// 		printf("%d   ", arr[i]);
+	// }
+	int a ;
+	a = (10,20,30);
+	printf("%d", a);
 }
 	
